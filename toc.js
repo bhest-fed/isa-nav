25c072e4faf7374d0630e2cb4f7780951d202989
bhest-fedgit config --global user.email bhester-fed@gmail.com <byron.hester.13@gmail.com>
2016-12-18 00:51:21 -0600

    Add page names to TOC labels

--- toc.js
+++ toc.js
@@ -1,40 +1,42 @@
 var toc = (function() {
     var _links = {
         // Global Content links
-        1000 : 'PII',
-        2000 : 'ISI',
-        3000 : 'REF',
-        3003 : '3DK',
+        1000: 'PRI',
+        2000: 'ISI',
+        3000: 'REF',
+        3003: '3DK',
+        3001: 'CVI', // Crosslinking Video
+        3002: 'MEV',
 
         // Slide Links
-        1: 'LAU', // Launch Pad
-        2: 'CVH', // Core Visual Aid Home
-        3: 'INT', // Introduction
-        4: 'EFH', // Efficacy Home
-        5: 'SEF', // SYNVISC Efficacy
-        6: 'SOE', 
-        7: 'EVH',
-        8: 'SVP',
-        9: 'SOE',
-        10: 'SVH',
-        11: 'SVI',
-        12: 'SVN',
-        13: 'EXH',
-        14: 'SCI',
-        15: 'VCO',
-        16: 'CRO',
-        17: 'CVI',
-        18: 'SAE',
-        19: 'SOA',
-        20: 'OAK',
-        21: 'DOS-REF',
-        22: 'PSU',
-        23: 'DAH',
-        24: 'REF',
-        25: 'MHO',
-        26: 'BIH',
+        1 : 'LAU', // Launch Pad
+        2 : 'CVH', // Core Visual Aid Home
+        3 : 'INT', // Introduction
+        4 : 'EFH', // Efficacy Home
+        5 : 'SEF', // SYNVISC Efficacy
+        6 : 'SOE', // Synvisc-One Efficacy
+        7 : 'EVH', // Evidence Home
+        8 : 'SVP', // SYNVISC Versus Placebo
+        9 : 'SOE', // Synvisc-One Evidence
+        10: 'SVH', // SYNVISC Versus Hyalgan
+        11: 'SVI', // SYNVISC Versus ICS
+        12: 'SVN', // SYNVISC Versus NSAIDS
+        13: 'EXH', // Experience Home
+        14: 'SCI', // Science
+        15: 'VCO', // Viscosupplement Comparison
+        16: 'CRO', // Crosslinking 
+        17: 'SAH', // Safety Home
+        18: 'SAE', // SYNVISC AEs
+        19: 'SOA', // Synvisc-One AEs
+        20: 'OAK', // OAK Incidence
+        21: 'SAF_REF', // References
+        22: 'PSU', // Patient Support
+        23: 'DAH', // Dosing and Administration
+        24: 'DOS_REF', // References
+        25: 'MHO', // Manufacturing Excellence Home
+        26: 'BIH', // Bibilography Home
         27: '',
-        28: '3DK',
+        28: '',
         29: ''
     };
 
